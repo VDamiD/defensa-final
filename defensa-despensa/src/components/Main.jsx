821e3ca2c49061fc78586f0f52b3408cbb72d4a5
VDamiD <damianda42@gmail.com>
2025-06-26 01:45:39 -0300

    Proyecto final despensa la vieja con botas

---
+++ defensa-despensa/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <Main>
+        
+    </Main>
+  )
+}
+
+export default Main
---
+++ defensa-despensa/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Main from '../components/Main'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
