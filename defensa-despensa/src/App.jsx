7e3bae37e343c782908181a60431fa1440020735
VDamiD <damianda42@gmail.com>
2025-06-29 21:03:00 -0300

    Subo avances de login, navegacion y validacion.

--- defensa-despensa/src/App.jsx
+++ defensa-despensa/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Login from "./pages/Login";
 import Productos from "./pages/Productos";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import NavbarDespensa from "./components/NavbarDespensa";
+
 function App() {
   return (
     <>
+      <NavbarDespensa />
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/productos" element={<Productos />} />
       </Routes>
--- defensa-despensa/src/components/NavbarDespensa.jsx
+++ defensa-despensa/src/components/NavbarDespensa.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import {Navbar, Nav, Conteiner } from 'react-bootstrap';import { Link} from 'react-router-dom';
-import React from 'react'
-
-export const NavbarDespensa = () => {
+import React from "react";
+import { Form, Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+const NavbarDespensa = () => {
   return (
-    <div>NavbarDespensa</div>
-  )
-}
-
-export default NavbarDespensa
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/login">
+            Despensa App
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/login">
+                Login
+              </Nav.Link>
+              <Nav.Link as={Link} to="/productos">
+                Productos
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+export default NavbarDespensa;
